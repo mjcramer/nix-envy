3bdee3287bd4d5caff5a049ef7d8495217b2a964
Michael Cramer <mjcramer@gmail.com>
2024-12-03 20:26:16 -0800

    init

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "Cramer's darwin system";
+
+  # format https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html#examples
+  inputs = {
+    # nixpkgs for packages
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+    # home-manager = {
+    #   url = "github:nix-community/home-manager/release-23.11";
+    #   inputs.nixpkgs.follows = "nixpkgs-darwin";
+    # };
+
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs-locked.url =
+    #   "github:NixOS/nixpkgs/1042fd8b148a9105f3c0aca3a6177fd1d9360ba5";
+
+    # nix-home-manager.url = "github:torgeir/nix-home-manager";
+    # dotfiles.url = "github:torgeir/dotfiles";
+  };
+
+  outputs = inputs@{ self, nixpkgs, darwin, home-manager, nix-home-manager, dotfiles, ... }: {
+    # Build darwin flake using:
+    # `darwin-rebuild build --flake .#jozibean`
+    darwinConfigurations = {
+      jozibean = nix-darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        specialArgs = { inherit inputs; };
+        modules = [
+          # {
+          #   nixpkgs.overlays = [
+          #     # pkgs.firefox-bin
+          #     inputs.nixpkgs-firefox-darwin.overlay
+
+          #     # use selected unstable packages with pkgs.unstable.xyz
+          #     # https://discourse.nixos.org/t/how-to-use-nixos-unstable-for-some-packages-only/36337
+          #     # "https://github.com/ne9z/dotfiles-flake/blob/d3159df136294675ccea340623c7c363b3584e0d/configuration.nix"
+          #     (final: prev: {
+          #       unstable =
+          #         import inputs.nixpkgs-unstable { system = prev.system; };
+          #     })
+
+          #     (final: prev: {
+          #       # pkgs.unstable-locked.<something>
+          #       unstable-locked =
+          #         import inputs.nixpkgs-locked { system = prev.system; };
+          #     })
+
+          #     (final: prev: {
+          #       # https://github.com/nix-community/home-manager/issues/1341#issuecomment-1468889352
+          #       mkAlias =
+          #         inputs.mkAlias.outputs.apps.${prev.system}.default.program;
+          #     })
+
+          #   ];
+          # }
+          ./modules/nix-core.nix
+          ./modules/system.nix
+          # ./modules/apps.nix
+          ./modules/host-users.nix
+          # home-manager.darwinModules.home-manager
+          # {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #   home-manager.users.torgeir = import ./home;
+          #   home-manager.extraSpecialArgs = {
+          #     inherit inputs;
+          #     dotfiles = dotfiles;
+          #     # hack around nix-home-manager causing infinite recursion
+          #     isLinux = false;
+          #   };
+          # }
+        ];
+      };
+    };
+  };
+}
---
+++ modules/host-users.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+let
+  hostname = "jozibean";
+  username = "cramer";
+in {
+  # Set the hostname
+  networking.hostName = hostname;
+  networking.computerName = hostname;
+  system.defaults.smb.NetBIOSName = hostname;
+
+  users.users."${username}" = {
+    home = "/Users/${username}";
+    description = username;
+  };
+}
