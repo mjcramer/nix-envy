05f39e6fd39e967b55a1c55c0a0adaee09cc247b
Michael Cramer <mjcramer@gmail.com>
2025-06-26 09:22:44 -0700

    fix sudo

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -2,16 +2,22 @@
 let 
   username = vars.username;
 in {
-   users = {
+  users = {
     users."${username}" = {
       isNormalUser = true;
       isSystemUser = false;
       home = "/Users/${username}";
       description = "User account for ${username}";
       group = username;
+      extraGroups = [ 
+        "wheel" 
+        "networkmanager" 
+        "docker" 
+      ];
       shell = pkgs.fish;
     };
     groups."${username}" = {};
   };
+  security.sudo.wheelNeedsPassword = false;
   nix.settings.trusted-users = [ username ];
 }
